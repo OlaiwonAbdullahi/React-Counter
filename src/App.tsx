64f23f03a885dabde7d5d390a978fe39a4a5600a
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-02 17:11:31 +0100

    update README.md and App.tsx update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div>
@@ -8,20 +10,21 @@ function App() {
 export default App;
 
 function Counter() {
-  const;
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
 
   return (
     <div>
       <div className="step">
-        <button onClick={handleInc}>-</button>
-        <span>Step:</span>
-        <button>+</button>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
 
       <div className="count">
-        <button>-</button>
-        <span>Count: </span>
-        <button>+</button>
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <span>Count:{count} </span>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
       </div>
     </div>
   );
