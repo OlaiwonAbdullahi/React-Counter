b3363ef6bc6ec2bd1d840f69d46347f7b349205f
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-02 18:48:46 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,10 @@ export default App;
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
 
+  const date = new Date("Sept 2 2024");
+  date.setDate(date.getDate() + count);
   return (
     <div>
       <div className="step">
@@ -22,10 +24,21 @@ function Counter() {
       </div>
 
       <div className="count">
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count:{count} </span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today Is "
+            : count > 0
+            ? `${count} days from today is`
+            : `${count} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
